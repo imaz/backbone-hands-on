57b49bad81355c7a0b02286bfeac780a784015d3
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 13:54:17 +0900

    04: Collectionの実装 add に失敗した時にアラートを出す ほんとべんり！

--- js/app.js
+++ js/app.js
@@ -13,6 +13,10 @@ $(function(){
     add_counter += 1; // これaddのコールバックとかに設定できないのかな?
   });
 
+  schedules.on('invalid', function(model, message){
+    alert(message);
+  });
+
   schedules.add({title: 'Backbone.js ハンズオン', datetime: moment('2013-10-24 9:30')}, {validate: true});
   schedules.add({title: 'hokaccha さんを囲む会', datetime: moment('2013-14-24 18:30')}, {validate: true});
   schedules.add({title: 'Backbone.js ハンズオン', datetime: moment('2013-10-25 9:30')}, {validate: true});
