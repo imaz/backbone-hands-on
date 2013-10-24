da809770496234ba45f050872ba50b6d9293d33f
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 11:44:17 +0900

    03: Modelのイベントや機能の実装 モデルの変更を監視するようにイベント設定した

--- js/app.js
+++ js/app.js
@@ -3,10 +3,14 @@ window.App = {};
 $(function(){
   var schedule = new App.Schedule;
 
+  schedule.on('change', function(){ // change:title というような指定もできる！
+    $('#schedule').html(
+      schedule.get('datetime').format('MM月DD日 HH時mm分') + ' : ' + schedule.get('title')
+    );
+  });
+
   schedule.set({
     title: 'Backbone.js ハンズオン',
     datetime: moment('2013-10-24 9:30')
   });
-
-  $('#schedule').html(schedule.get('datetime').format('MM月DD日 HH時mm分') + ' : ' + schedule.get('title'));
 });
