47e068edc3ddc6a4b4d4b4fbf3de3b102d99ef66
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 13:33:37 +0900

    04: Collectionの実装 Collection も同じように validate をかけられる

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 window.App = {};
 
 $(function(){
+  var schedules = new App.Schedules();
+
+  schedules.add({title: 'Backbone.js ハンズオン', datetime: moment('2013-10-24 9:30')}, {validate: true});
+  schedules.add({title: 'Backbone.js ハンズオン', datetime: moment('2013-10-25 9:30')}, {validate: true});
+  schedules.add({title: 'hokaccha さんを囲む会', datetime: moment('2013-10-34 18:30')}, {validate: true});
+
+  console.log(schedules.models);
+  console.log(schedules.pop());
+  console.log(schedules.models);
 });
