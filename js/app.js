81f5aa3c92bced4de3a054574b4969ee930cc0bc
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 15:53:36 +0900

    08: CalendarのViewクラスをつくる いらないコードをけしたったー

--- js/app.js
+++ js/app.js
@@ -3,45 +3,8 @@ window.App = {};
 $(function(){
   var schedules = new App.Schedules();
 
-  var appendSchedule = function(model){
-    var $li = $('<li>');
-    $li.html(model.show());
-    $('.list').append($li)
-  };
-
   var createFormView = new App.CreateFormView({
     el: '.createForm', // this に入ってくるのかー!!
     collection: schedules
-  });
-
-  $('.createForm').on('submit', function(e){
-    e.preventDefault();
-
-    var title = $('input[name="title"]').val() || undefined; // デフォルト値が入るようにする(良くないコードらしい)
-    var datetime = moment($('input[name="datetime"]').val());
-
-    schedules.add({title: title, datetime: datetime}, {validate: true});
-  });
-
-  $('.filterForm').on('submit', function(e){
-    e.preventDefault();
-
-    var filterDate = $('input[name="filterDate"]').val();
-    var results = schedules.findByDate(filterDate);
-
-    $('.count').html(results.length + '件の予定があります');
-    $('.list').empty();
-
-    _.each(results, function(model){
-      appendSchedule(model);
-    });
-  });
-
-  schedules.on('invalid', function(model, message){
-    alert(message);
-  });
-
-  schedules.on('add', function(model){
-    appendSchedule(model);
   });
 });
