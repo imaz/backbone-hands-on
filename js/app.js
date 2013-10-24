ce2eccc90cf3af4e2423e7e8f41d5f70c4574fcb
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 13:22:14 +0900

    04: Collectionの実装 app.js の中身を消した

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
 window.App = {};
 
 $(function(){
-  var schedule = new App.Schedule;
-
-  schedule.on('change', function(){ // change:title というような指定もできる！
-    $('#schedule').html(
-      schedule.formatDateTime('MM月DD日 HH時mm分') + ' : ' + schedule.get('title')
-    );
-  });
-
-  schedule.on('invalid', function(model, message){
-    alert(message);
-  });
-
-  schedule.set({
-    title: 'Backbone.js ハンズオン',
-    datetime: moment('2013-10-24 9:30')
-  }, {validate: true});
 });
