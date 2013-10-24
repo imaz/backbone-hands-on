a027c724afbce1157186510297cce969ea5c79ee
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 14:28:29 +0900

    datetime の validate エラーを修正

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ $(function(){
     var title = $('input[name="title"]').val();
     var datetime = $('input[name="datetime"]').val();
 
-    schedules.add({title: title, datetime: datetime});
+    schedules.add({title: title, datetime: moment(datetime)});
+  });
 
     console.log(schedules.pop());
   });
