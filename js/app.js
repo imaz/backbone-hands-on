472fe1e3841a12db242f89587c41c2ceb2fc8beb
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-24 14:07:40 +0900

    05: Formから予定を追加する Form の submit イベントハンドラを作った

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 window.App = {};
 
 $(function(){
+  $('.createForm').on('submit', function(e){
+    e.preventDefault();
+
+    var title = $('input[name="title"]').val()
+    , datetime = $('input[name="datetime"]').val()
+    ;
+
+    console.log(title, datetime);
+  });
 });
