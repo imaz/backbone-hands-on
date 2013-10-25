03d263ac6cb33dd006017ce79c426c43cb86f233
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-10-25 10:03:55 +0900

    11: カレンダーに予定を描画する ダミーデータを作成

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ window.App = {};
 $(function(){
   var schedules = new App.Schedules();
 
+  // dummy data
+  schedules.add([
+    {title: 'Backbone.js ハンズオン', datetime: moment('2013-10-24 9:30')},
+    {title: 'Backbone.js ハンズオン', datetime: moment('2013-10-25 9:30')},
+    {datetime: moment('2013-09-30 8:00')}
+  ])
+
   var createFormView = new App.CreateFormView({
     el: '.createForm', // this に入ってくるのかー!!
     collection: schedules
